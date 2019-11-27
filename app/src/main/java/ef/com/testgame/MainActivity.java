9432874b216317f5f2068a2bbd5590fe25b267db
levanloi911 <54982847+levanloi911@users.noreply.github.com>
2019-11-27 15:27:00 +0700

    code anh huy 27/11/2019

--- app/src/main/java/ef/com/testgame/MainActivity.java
+++ app/src/main/java/ef/com/testgame/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
@@ -15,7 +16,7 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private RecyclerView rcvTable;
-    private static final int TABLE_SIZE = 5;
+    private int tablesize;
     private List<Integer> list = new ArrayList<>();
     private int positionEmpty;
     private ListAdapter valuesAdapter;
@@ -25,9 +26,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         rcvTable= findViewById(R.id.activity_main_rcv_table);
-        randomList();
 
-        valuesAdapter = new ListAdapter(list, TABLE_SIZE, new ListAdapter.ListAdapterListener() {
+        Intent mIntent = getIntent();
+        tablesize = mIntent.getIntExtra("TABLE_SIZE", 0);
+
+        randomList();
+        valuesAdapter = new ListAdapter(list, tablesize, new ListAdapter.ListAdapterListener() {
             @Override
             public void onItemClick(int positon) {
                 handleWhenClickItem(positon);
@@ -35,26 +39,27 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         rcvTable.setHasFixedSize(true);
-        GridLayoutManager gridLayoutManager = new GridLayoutManager(this, TABLE_SIZE);
+            GridLayoutManager gridLayoutManager = new GridLayoutManager(this, tablesize);
         rcvTable.setLayoutManager(gridLayoutManager);
         rcvTable.setAdapter(valuesAdapter);
     }
 
     private void randomList() {
-        int size= TABLE_SIZE * TABLE_SIZE;
+        int size= tablesize * tablesize -1;
         for (int i = 0; i < size; i++) {
-            list.add(i);
+            list.add(i+1);
         }
         Collections.shuffle(list);
+        list.add(0);
         positionEmpty= list.indexOf(0);
 
     }
 
     private void handleWhenClickItem(int potision){
-        if(((positionEmpty+1)%TABLE_SIZE !=1 && (potision==positionEmpty-1))
-                || ((positionEmpty+1)%TABLE_SIZE !=0 && (potision==positionEmpty+1))
-                || potision==positionEmpty+TABLE_SIZE
-                || potision==positionEmpty-TABLE_SIZE){
+        if(((positionEmpty+1)%tablesize !=1 && (potision==positionEmpty-1))
+                || ((positionEmpty+1)%tablesize !=0 && (potision==positionEmpty+1))
+                || potision==positionEmpty+tablesize
+                || potision==positionEmpty-tablesize){
 
             list.set(positionEmpty,list.get(potision));
             list.set(potision,0);
@@ -66,7 +71,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void checkEndGame(){
-        int size= TABLE_SIZE * TABLE_SIZE;
+        int size= tablesize * tablesize;
         for(int i=0; i<size-1; i++){
             if(list.get(i)!=i+1){
                 break;
